e974d11c8abc0bdf750a97a3613baa803f062bd6
Kabal1 <mert2009@hotmail.com>
2015-04-04 22:14:10 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,13 +5,27 @@ using System.Text;
 using System.Threading.Tasks;
 using LeagueSharp;
 
-namespace ConsoleApplication6
+namespace HelloWorld
 {
     class Program
     {
+        private static Spell Q, W, E, R;
+
         static void Main(string[] args)
         {
-            Game.PrintChat("Hello");
+            CustomEvents.Game.OnGameLoad += Game_OnGameLoad;
         }
+        static void Game_OngameLoad(EventArgs args)
+        {
+            if (Player.ChampionName != "Graves")
+            {
+                return;
+            }
+            else
+            {
+                Game.PrintChat("Graves 1.0");
+            }
+        }
+        
     }
 }
