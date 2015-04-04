ceab1aa97d54600cd2801499efa10ae142408cc6
Kabal1 <mert2009@hotmail.com>
2015-04-04 22:38:10 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -12,6 +12,7 @@ namespace HelloWorld
     {
         private static Orbwalking.Orbwalker Orbwalker;
         private static Spell Q, W, E, R;
+        private static Menu Menu;
 
         static void Main(string[] args)
         {
@@ -23,6 +24,14 @@ namespace HelloWorld
             W = new Spell(SpellSlot.W, 700); // create W spell with a range of 700 units
             E = new Spell(SpellSlot.E, 550); // create E spell with a range of 550 units
             R = new Spell(SpellSlot.R, 650); // create R spell with a range of 650 units
+            Menu = new Menu(ObjectManager.Player.ChampionName, ObjectManager.Player.ChampionName, true);
+            Menu orbwalkerMenu = Menu.AddSubMenu(new Menu("Orbwalker", "Orbwalker"));
+            Orbwalker = new Orbwalking.Orbwalker(orbwalkerMenu);
+            Menu ts = Menu.AddSubMenu(new Menu("Target Selector", "Target Selector")); ;
+            TargetSelector.AddToMenu(ts);
+            Menu spellMenu = Menu.AddSubMenu(new Menu("Spells", "Spells"));
+            spellMenu.AddItem(new MenuItem("useQ", "Use Q").SetValue(true));
+
             if (ObjectManager.Player.ChampionName != "Graves")
             {
                 return;
@@ -45,7 +54,7 @@ namespace HelloWorld
         {
             // check if the player wants to use E
             //if (!Menu.Item("useE").GetValue<bool>())
-             //   return;
+            //   return;
 
             // gets best target in Dfg(750) / E(550)
             Obj_AI_Hero target = TargetSelector.GetTarget(750, TargetSelector.DamageType.Magical);
